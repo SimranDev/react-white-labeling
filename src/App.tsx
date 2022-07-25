9fe38fbf88b66f76f16fd624630aa75976458d31
Simran <simran@tella.co.nz>
2022-07-25 15:46:44 +1200

    basic styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex">
+      <div className="border-4 border-red-500 text-red-400 grid items-center justify-center h-min w-max m-auto p-4 text-center rounded-lg">
+        <h1>White Labeling</h1>
+        <h6>based on sub domains</h6>
+      </div>
     </div>
   );
 }
