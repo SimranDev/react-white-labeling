7778c939ab039148c82256333e9cc739fe9c4d50
Simran <simran@tella.co.nz>
2022-07-26 14:01:33 +1200

    useTheme hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
   const [subDomain] = useState<string>(window.location.hostname.split(".")[0]);
 
-  const themes = {
-    tella: "text-pink-700 border-pink-700",
-    eightfold: "text-purple-700 border-purple-700",
-    default: "",
-  };
+  const { theme, bgColor, textColor } = useTheme();
 
   return (
     <div className="h-screen flex">
-      <div className="m-auto scale-150 grid gap-2">
+      <div
+        className="m-auto scale-150 grid gap-2"
+        style={{ color: `${bgColor}` }}
+      >
+        <code className=" bg-gray-500 text-white text-xs p-1 rounded-md scale-75">
+          Theme State : <br /> {JSON.stringify(theme)}
+        </code>
         <div
-          className={`border-4 p-4 text-center rounded-lg ${
-            themes[subDomain as keyof typeof themes]
-          }`}
+          className={`border-4 p-4 text-center rounded-lg`}
+          style={{ borderColor: `${textColor}` }}
         >
           <h1>White Labeling</h1>
           <h6>based on sub domains</h6>
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export type Theme  ={
+    bgColor: string;
+    textColor: string;
+}
+
+enum WhiteLabelClient {
+    TELLA = 'tella',
+    EIGHTFOLD = 'eightfold',
+}
+
+const themes:Record<WhiteLabelClient, Theme> = {
+    tella: {
+        bgColor: "red",
+        textColor: "black",
+    },
+    eightfold: {
+        bgColor: "green",
+        textColor: "darkseagreen",
+    },
+}
+
+export const useTheme = () => {
+    const [subDomain] = useState<WhiteLabelClient>(window.location.hostname.split(".")[0] as WhiteLabelClient);
+    const [theme, setTheme] = useState<Theme>(themes['tella']);
+
+    useEffect(() => {
+        setTheme(themes[subDomain] || themes[WhiteLabelClient.TELLA]);
+    }, [subDomain])
+
+    const {bgColor, textColor} = theme;
+
+    return { theme, bgColor, textColor };
+}
